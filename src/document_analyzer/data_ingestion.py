16a200a7e9dfcf539a70f7e7f1b71659fbae3682
jndumu <latinue@gmail.com>
2025-07-28 00:00:35 -0400

    data analysis updated

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     from pathlib import Path
     from io import BytesIO
     
-    pdf_path=r"C:\Users\Dell\Desktop\document_portal\notebook\data\RAG_with_knowledge_intensive_task.pdf"
+    pdf_path=r"C:\Users\Dell\Desktop\document_portal\data\document_analysis\test_session\sample.pdf"
     class DummnyFile:
         def __init__(self,file_path):
             self.name = Path(file_path).name
